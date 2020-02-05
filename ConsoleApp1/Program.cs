2dcca33613a9475a76e66e95c8f1868fd2f78efc
Nadezhda Shatalova <alonewithlight@yandex.ru>
2020-02-06 01:00:02 +0400

    console part is working now

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,32 +5,32 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static void GenerateDecimal(string s)
+        {
+            string[] numbers =  s.Split(new char[] {','},StringSplitOptions.RemoveEmptyEntries);
+            decimal x = System.Convert.ToDecimal(numbers[0].Trim(' '));
+            decimal y = System.Convert.ToDecimal(numbers[1].Trim(' '));
+            Console.WriteLine("X:{0}, Y:{1}",x,y); 
+        }
+        
         static void Main(string[] args)
         {
            ArrayList list = new ArrayList();
-           string str = Console.ReadLine();
 
-            while (str != null && !str.Equals(""))
+            while (true)
             {
-                str = Console.ReadLine();
-                if (str!="")
+                string str = Console.ReadLine();
+                if (str=="") break;
                 list.Add(str);
             }
-            
-            foreach (string v in list)
-                Console.WriteLine(v);
-            
+           
             Console.WriteLine("Координаты Х и У:");
             try
             {
                 foreach (string v in list)
                 {
-                    string[] numbers =  v.Split(new char[] {','},StringSplitOptions.RemoveEmptyEntries);
-                    decimal x = System.Convert.ToDecimal(numbers[0].TrimStart(' ').Replace('.',','));
-                    decimal y = System.Convert.ToDecimal(numbers[1].TrimStart(' ').Replace('.',','));
-                    Console.WriteLine("X:{0}, Y:{1}",x,y);
+                    GenerateDecimal(v);
                 }
-
             }
             catch (Exception e)
             {
