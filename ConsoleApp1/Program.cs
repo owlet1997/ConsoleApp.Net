95d2336bf0423537c16f63330d4248e142c4f27c
Nadezhda Shatalova <alonewithlight@yandex.ru>
2020-02-10 23:02:16 +0400

    + третья работа - все, кроме юнит-тестов

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,187 +3,124 @@ using System.Collections;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class NewtonRoot
-    {
-        private double startNumber;
-        const double EPS = Double.Epsilon;
-        private int n;
-        private double calculatedResult;
-        private double mathPowResult;
-        private double dimension;
-        
-        public NewtonRoot(double startNumber, int n)
-        {
-            this.startNumber = startNumber;
-            this.n = n;
-            calculatedResult = CalculateRoot();
-            mathPowResult = Math.Pow(startNumber,(double) 1/n);
-            dimension = mathPowResult - calculatedResult;
-        }
-
-        public double StartNumber => startNumber;
-
-        public int N => n;
-
-        public double CalculatedResult => calculatedResult;
-        
-        public double MathPowResult => mathPowResult;
-
-        public double Dimension => dimension;
-
-        private double CalculateRoot()
-        {
-            double x0 = startNumber / n;
-            double xk = 1;
-            int count = 0;
-            double A = startNumber;
-            while (Math.Abs(x0 - xk)>EPS)
-            {
-                if (count>0) x0 = xk;
-                xk = (n - 1) * x0 / n + A / (n * Math.Pow(x0, n - 1)); 
-                //Console.WriteLine($"xk = {xk}");
-                count++;
-            }
-            return xk;
-        }
-    }
-
-    class ConvertToBinary
-    {
-        private int startNumber;
-        private string calcResult;
-        private string functionResult;
-
-        public string FunctionResult => functionResult;
-
-        public ConvertToBinary(int startNumber)
-        {
-            this.startNumber = startNumber;
-            calcResult = ConvertBinary();
-            functionResult = Convert.ToString(startNumber, 2);
-        }
-
-        public int StartNumber => startNumber;
-
-        public string CalcResult => calcResult;
-
-        private string ConvertBinary()
-        {
-            string str = "";
-            StringBuilder builder = new StringBuilder(str);
-            int x = startNumber;
-            while (x > 0)
-            {
-                int temp = x % 2;
-                x/=2;
-                builder.Append(temp);
-            }
-
-            char[] array = builder.ToString().ToCharArray();
-            Array.Reverse(array);
-            return new string(array);
-        }
-    }
     class Program
     {
-        static NewtonRoot GetObject()
+        static NODEvklid GetInstanceTwo()
         {
-            double number;
-            while (true)
+            int n = 2;
+            int[] numbers = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("Введите число х:");
-                try
-                {
-                    number = Double.Parse(Console.ReadLine());
-                    if (number < 0)
-                    {
-                        Console.WriteLine("Число должно быть положительным");
-                    }
-                    else break;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Вы должны ввести число, а не строку");
-                }
+                numbers[i] = random.Next((i+1)*100+1,  100*(i+2));
             }
 
+            NODEvklid evklid = new NODEvklid(numbers[0], numbers[1]);
+            return evklid;
+
+        }
+        static NODEvklid GetInstanceThreeOrMore()
+        {
             int n;
             while (true)
             {
-                Console.WriteLine("Введите n - корень какой степени хотите извлечь:");
+                Console.WriteLine("Введите количество чисел");
                 try
                 {
                     n = Int32.Parse(Console.ReadLine());
-                    if (n < 0)
+                    if (n < 3 || n > 5)
                     {
-                        Console.WriteLine("Степень должна быть положительной");
+                        Console.WriteLine("Мы считаем НОД у чисел от трех до пяти");
                     }
-                    else break;
+                    else
+                    {
+                        break;
+                    }
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("Вы должны ввести число, а не строку");
+                    Console.WriteLine("Ошибка ввода");
                 }
             }
-            NewtonRoot root = new NewtonRoot(number,n);
-            return root;
+            int[] numbers = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = random.Next((i+1)*100+1,  100*(i+2));
+            }
+
+            NODEvklid evklid = null;
+            switch (n)
+            { 
+              case 3:  evklid = new NODEvklid(numbers[0], numbers[1], numbers[2]);
+                  break;
+              case 4:  evklid = new NODEvklid(numbers[0], numbers[1], numbers[2], numbers[3]);
+                  break;
+              case 5:  evklid = new NODEvklid(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]);
+                  break;
+              default:
+                  Console.WriteLine("Ошибка!");
+                  break;
+            }
+            
+            return evklid;
         }
+        
+        
 
         static void Task1()
         {
-            NewtonRoot root = GetObject();
-            Console.WriteLine($"Вычисление корня {root.N}-й степени из числа {root.StartNumber}");
-            Console.WriteLine($"Значение, полученное при помощи метода Ньютона: {root.CalculatedResult}");
-            Console.WriteLine($"Значение, полученное при помощи метода Math.Pow: {root.MathPowResult}");
-            Console.WriteLine($"Разница в значениях: {root.Dimension}");
-        }
-
-        static ConvertToBinary GetInstance()
-        {
-            int n;
-            while (true)
-            {
-                Console.WriteLine("Введите число n:");
-                try
-                {
-                    n = Int32.Parse(Console.ReadLine());
-                    if (n < 0)
-                    {
-                        Console.WriteLine("Число должно быть положительным");
-                    }
-                    else break;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Вы должны ввести число, а не строку");
-                }
-            } 
-            ConvertToBinary obj = new ConvertToBinary(n);
-            return obj;
-        }
-
-        static void Task2()
-        {
-            ConvertToBinary binary = GetInstance();
-            Console.WriteLine($"Вычисленное двоичное представление числа {binary.StartNumber} по написанному алгоритму: {binary.CalcResult}");
-            Console.WriteLine($"Вычисленное двоичное представление числа {binary.StartNumber} по встроенной функции: {binary.FunctionResult}");
+            NODEvklid nodEvklid = GetInstanceTwo();
+            nodEvklid.setDivisor(nodEvklid.NODBinaryTime());
+            Console.WriteLine($"Наибольший общий делитель чисел {nodEvklid.A} и {nodEvklid.B} : {nodEvklid.Divisor}");
         }
         
+        static void Task2()
+        {
+            NODEvklid nodEvklid = GetInstanceTwo();
+            nodEvklid.setDivisor(nodEvklid.NODEvklidTime());
+            Console.WriteLine($"Наибольший общий делитель чисел {nodEvklid.A} и {nodEvklid.B} : {nodEvklid.Divisor}");
+
+        }
+
+        static void Task3()
+        {
+            NODEvklid nodEvklid = GetInstanceThreeOrMore();
+            switch (nodEvklid.N)
+            {
+               case 3: 
+                   Console.WriteLine($"Наибольший общий делитель чисел {nodEvklid.A}, {nodEvklid.B} и {nodEvklid.C} : {nodEvklid.Divisor}");
+                    break;
+               case 4:
+                   Console.WriteLine($"Наибольший общий делитель чисел {nodEvklid.A}, {nodEvklid.B}, {nodEvklid.C} и {nodEvklid.D} : {nodEvklid.Divisor}");
+                   break;
+               case 5:
+                   Console.WriteLine($"Наибольший общий делитель чисел {nodEvklid.A}, {nodEvklid.B}, {nodEvklid.C}, {nodEvklid.D} и {nodEvklid.E} : {nodEvklid.Divisor}");
+                   break;
+               default:
+                   Console.WriteLine("oooooooops! smth went wrong!");
+                   break;
+            }
+        }
+
         static int ShowStartMenu()
         {
             int number;
             while (true)
             {
-                Console.WriteLine("1 - Задание 1, вычисление корня числа методом Ньютона");
-                Console.WriteLine("2 - Задание 2, разработка алгоритма конвертации десятичного числа в строку, содержащую двоичное представление");
-                Console.WriteLine("3 - Выход");
+                Console.WriteLine("");
+                Console.WriteLine("1 - Задание 1, вычисление НОД методом Евклида");
+                Console.WriteLine("2 - Задание 2, вычисление НОД методом Стейна");
+                Console.WriteLine("3 - Задание 3, вычисление НОД методом у 3 - 5 чисел");
+                Console.WriteLine("4 - Выход");
                 try
                 {
                     number = Int32.Parse(Console.ReadLine());
-                    if (number > 3 || number < 1)
+                    if (number > 4 || number < 1)
                     {
                         Console.WriteLine("Число не предусмотрено");
                     }
@@ -197,23 +134,32 @@ namespace ConsoleApp1
                     Console.WriteLine("Ошибка ввода");
                 }
             }
+            Console.WriteLine("");
             return number;
         }
-
+        
         static void Main(string[] args)
         {
-            int chosen = ShowStartMenu();
-            switch (chosen)
+            while (true)
             {
-                case 1: Task1();
-                    break;
-                case 2: Task2();
-                    break;
-                case 3: Console.WriteLine("Сеанс окончен");
-                    break;
-                default:
-                    break;
+                int chosen = ShowStartMenu();
+                switch (chosen)
+                {
+                    case 1: Task1();
+                        break;
+                    case 2:
+                        Task2();
+                        break;
+                    case 3:
+                        Task3();
+                        break;
+                    case 4: Console.WriteLine("Сеанс окончен");
+                        return;
+                    default:
+                        break;
+                } 
             }
+           
         }
     }
 }
